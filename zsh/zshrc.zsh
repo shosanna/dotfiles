d3c831f26d96c50c17805c19614e21dae727729b
Zuzana Dostalova <dostalovaz@gmail.com>
2017-02-07 21:49:44 +0100

    Distraction free writting

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -171,6 +171,6 @@ alias rdms="rake db:drop db:create db:migrate db:seed"
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
-base16_flat
+base16_mocha
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
