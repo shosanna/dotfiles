eec4bec0f5bd0268579e4ff0dd3c03576c0b2dd0
Zuzana Dostalova <dostalovaz@gmail.com>
2017-03-12 01:01:22 +0100

    Unikitty dark scheme!

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -170,7 +170,7 @@ alias pe="pacman -Ss"
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
-base16_mocha
+base16_unikitty-dark
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
