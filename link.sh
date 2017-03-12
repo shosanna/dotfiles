e7cc68eb61b444bf0e3b18f5f149d16a45d1bbae
Zuzana Dostalova <dostalovaz@gmail.com>
2017-03-12 02:07:47 +0100

    Move xinit and fehbg to dotfiles

--- link.sh
+++ link.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-ln -nsf $HOME/.dotfiles/tmux.conf      $HOME/.tmux.conf
-ln -nsf $HOME/.dotfiles/tmuxinator     $HOME/.tmuxinator
-ln -nsf $HOME/.dotfiles/vim            $HOME/.vim
-ln -nsf $HOME/.dotfiles/vim/vimrc      $HOME/.vimrc
-ln -nsf $HOME/.dotfiles/zsh            $HOME/.oh-my-zsh
-ln -nsf $HOME/.dotfiles/zsh/zshrc.zsh  $HOME/.zshrc
+ln -nsf "$HOME/.dotfiles/tmux.conf"     "$HOME/.tmux.conf"
+ln -nsf "$HOME/.dotfiles/tmuxinator"    "$HOME/.tmuxinator"
+ln -nsf "$HOME/.dotfiles/vim"           "$HOME/.vim"
+ln -nsf "$HOME/.dotfiles/vim/vimrc"     "$HOME/.vimrc"
+ln -nsf "$HOME/.dotfiles/zsh"           "$HOME/.oh-my-zsh"
+ln -nsf "$HOME/.dotfiles/zsh/zshrc.zsh" "$HOME/.zshrc"
+ln -nsf "$HOME/.dotfiles/xinitrc"       "$HOME/.xinitrc"
+ln -nsf "$HOME/.dotfiles/fehbg"         "$HOME/.fehbg"
